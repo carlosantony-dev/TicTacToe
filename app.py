377ee80e15480c92915aef0d6244f3e9784b2d55
Carlos <carlosblecha18@gmail.com>
2022-09-23 22:37:30 -0300

    Create inital methods

---
+++ app.py
@@ -0,0 +1,24 @@
+class TicTacToe:
+
+    def __init__(self):
+        self.board = []
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+    
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+    def start(self):
+        self.create_board()
+        self.show_board()
+
+tictactoe = TicTacToe()
+tictactoe.start()
